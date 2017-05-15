2a1a15c1ddbc60bf167d3003d95f831ae44ded3a
mbali17 <alimirbasheer@gmail.com>
2017-05-15 01:06:07 -0500

    variables in python

---
+++ Variables.py
@@ -0,0 +1,9 @@
+#variables in python do not have a type they just have names and it can hold any value.
+exVar= 5;
+print(exVar)
+opVar=exVar/2
+print(opVar)
+
+#The names of the variables cannot start with a number and if there is really a need for using number at the beginning we can use the "_" charachter
+_100Var=100;
+print(_100Var)
