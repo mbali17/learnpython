2d380d34a38d59e5f82dd232f5b63ce514fda52f
mbali17 <alimirbasheer@gmail.com>
2017-05-15 01:35:14 -0500

    for loop

---
+++ forloop.py
@@ -0,0 +1,8 @@
+exampleList = [1,24,5,677,77,676]
+#Iterating over list.
+for element in exampleList:
+    print(element)
+
+#creating counter using for loop this loops from 1 upto 10 and prints them.
+for x in range(1,11):
+    print(x)
