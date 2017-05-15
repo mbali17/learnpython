cfced3c95384dceb4c2e55efcf48a348e3a59786
mbali17 <alimirbasheer@gmail.com>
2017-05-15 01:19:47 -0500

    while loop and commenting

---
+++ WhileLoop.py
@@ -0,0 +1,15 @@
+condition = 1;
+#Read this as "Loop as long as value of condition is less than 10"  this uses the conditional operator.
+while condition < 10:
+    print(condition)
+    #This is a shorthand for condition = condition+1
+    condition+=1;
+
+#Infinite while loop.
+while True:
+    print("This will be printed inifinitely")
+
+'''
+This is a
+multiline commit 
+'''
