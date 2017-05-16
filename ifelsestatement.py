274558a351c05a80ee23a90b1f59834b443c9b63
mbali17 <alimirbasheer@gmail.com>
2017-05-15 21:45:49 -0500

    if else statement

---
+++ ifelsestatement.py
@@ -0,0 +1,11 @@
+x = 13
+y = 6
+#The else statement is associated with the last 'if' block.
+#if there are multiple if blocks stacked.  
+if x < y:
+    print(x,'is less than',y)
+#Un comment and execute to see in working the meaning of the above statement.
+#if x>y:
+ #   print(x,'is greater than',y)
+else:
+    print(x,'is not less than',y)
