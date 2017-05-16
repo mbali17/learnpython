362ff6e3ffbad11231a5b11483ef9a166d3c951e
mbali17 <alimirbasheer@gmail.com>
2017-05-15 22:35:02 -0500

    functions in pyhon

---
+++ functionsinpython.py
@@ -0,0 +1,12 @@
+'''
+A function is a collection of code which can be grouped and invoked by the name.
+to declare a function we use the 'def' keyword followed by the name of the
+function and braces which tell the parameters to be passed, if this is empty
+then it means the function does not expect any parameters.
+'''
+def exampleFunction():
+    x=3
+    y=33;
+    print(x+y)
+
+exampleFunction()
