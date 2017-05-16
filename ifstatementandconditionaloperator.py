b2e16623adb27f05238529e927ea15f9808713ba
mbali17 <alimirbasheer@gmail.com>
2017-05-15 21:32:53 -0500

    if statement and conditional operator

---
+++ ifstatementandconditionaloperator.py
@@ -0,0 +1,13 @@
+x=2
+y=7
+z=10
+# the conditional operators are '<' '<=' '>' '>=' '==' and an
+#if statement is used check a particular condition and execute the body only once
+if x<y:
+    print(x,'is less than',y)
+
+if y>x:
+    print(y,'is greater than',x)
+
+if z>y>x:
+    print(z,'is greater than ',y,'which is greater than',x)
