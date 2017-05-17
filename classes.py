9e5094da050f126b4d40d695ef9bd4243979fec2
mbali17 <alimirbasheer@gmail.com>
2017-05-16 21:32:36 -0500

    classes

---
+++ classes.py
@@ -0,0 +1,20 @@
+#classes in pthon are defined using the class keyword and is collection of mehods
+class calc:
+    answer = 0;
+    def add(x,y):
+        answer = x+y
+        return answer
+    def sub(x,y):
+        answer = x-y
+        return answer
+    def mult(x,y):
+        answer = x*y
+        return answer
+    def div(x,y):
+        answer = x/y
+        return answer
+
+print(calc.add(2,5))
+print(calc.sub(2,5))
+print(calc.mult(2,5))
+print(calc.div(2,5))
