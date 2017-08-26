75a2e589ddeae584cee7407d52913d610c1ab68a
mbali17 <alimirbasheer@gmail.com>
2017-08-26 15:13:42 -0500

    updated files

--- pythonbasics/Variables.py
+++ pythonbasics/Variables.py
@@ -4,6 +4,9 @@ print(exVar)
 opVar=exVar/2
 print(opVar)
 
-#The names of the variables cannot start with a number and if there is really a need for using number at the beginning we can use the "_" charachter
+#Variable names in python can begin only with letter or undersore
+#The names of the variables cannot start with a number and if there is really a need for using number at the beginning 
+# we can use the "_" charachter
 _100Var=100;
 print(_100Var)
+
